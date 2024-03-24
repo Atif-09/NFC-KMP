55e97e0d2f86826605356c2f08651de36ae885aa
Muhammad Atif <ma2669637@gmail.com>
2024-03-24 15:44:45 +0500

    added nfc code for android

--- composeApp/src/androidMain/kotlin/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/Platform.android.kt
@@ -1,7 +1,15 @@
+import android.content.Context
 import android.os.Build
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual class PlatformContext(val context: Context)
+
+@Composable
+actual fun getPlatformContext() = PlatformContext(LocalContext.current)
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,37 +1,63 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import nfcRead.NfcManager
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import nfcreaderapp.composeapp.generated.resources.Res
-import nfcreaderapp.composeapp.generated.resources.compose_multiplatform
-
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        var searchTag by remember { mutableStateOf(false) }
+        val scope = rememberCoroutineScope()
+        val nfcManager = NfcManager(getPlatformContext())
+        var tagData by remember { mutableStateOf("") }
+
+        scope.launch {
+
+            nfcManager.flow.collectLatest {
+                tagData = it
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        }
+        Box(Modifier.fillMaxSize()) {
+
+            Column(
+                modifier = Modifier.fillMaxWidth().align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                if (tagData.isNotEmpty()) {
+
+                    Text("NFC Tag Data:  ${tagData.substring(3,20)}")
+                }
+                Button(onClick = {
+                    searchTag = !searchTag
+                }, modifier = Modifier.fillMaxWidth().padding(horizontal = 18.dp)) {
+                    Text("Search Tag")
                 }
             }
+
+            if (searchTag) {
+                nfcManager.RegisterForNFC()
+            }
+
         }
     }
 }
